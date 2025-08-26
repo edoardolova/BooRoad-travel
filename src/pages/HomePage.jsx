40e1af7541cb9c107d709e06818528b0262da0ee
GianniC88 <gianni.colucci.88@gmail.com>
2025-08-26 10:34:15 +0200

    feat: add Jumbotron component to HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,11 @@
 import TripsList from "../components/TripsList";
+import Jumbotron from "../components/Jumbotron";
 
 export default function HomePage() {
     return (
         <>
-            <h1>HOME PAGE</h1>
-            <TripsList/>
+            <Jumbotron title="Boo Road Travel" text="Scopri le migliori destinazioni e pianifica il Tuo Viaggio" cta="Scopri i Viaggi" img_url="https://esempio.com/banner.jpg" />
+            <TripsList />
         </>
     )
 }
