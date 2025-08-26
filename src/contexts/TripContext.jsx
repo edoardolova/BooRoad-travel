046fa65c46ffd30d1d3145415cc13921b415367f
Edoardo Lova <edoardolova81@gmail.com>
2025-08-26 10:52:11 +0200

    feat: add addPartecipant, addTrip and addParticipantToTrip in TripContext

--- src/contexts/TripContext.jsx
+++ src/contexts/TripContext.jsx
@@ -5,24 +5,61 @@ import { trips as initialTrips, participants as initialParticipants } from "../d
 export const TripContext = createContext();
 
 export function TripProvider ({ children }){
-  // stati reattivi
-  const [trips, setTrips] = useState(initialTrips);
-  const [participants, setParticipants] = useState(initialParticipants);
+    // stati reattivi
+    const [trips, setTrips] = useState(initialTrips);
+    const [participants, setParticipants] = useState(initialParticipants);
 
-  // funzione helper per ottenere partecipanti di un viaggio
-  const getParticipantsByTripId = (tripId) => {
-    const trip = trips.find(t => t.id === tripId);
-    if (!trip){
-        return [];
-    } 
-    return participants.filter(p => trip.participants.includes(p.id));
-  };
+    // funzione helper per ottenere partecipanti di un viaggio
+    const getParticipantsByTripId = (tripId) => {
+        const trip = trips.find(t => t.id === tripId);
+        if (!trip){
+            return [];
+        } 
+        return participants.filter(p => trip.participants.includes(p.id));
+    };
 
-  return (
-    <TripContext.Provider value={{ trips, setTrips, participants, setParticipants, getParticipantsByTripId }}>
-      {children}
-    </TripContext.Provider>
-  );
+    // aggiungere un nuovo partecipante
+    const addParticipant = (newParticipant) => {
+        setParticipants(prev => [...prev, newParticipant]);
+    };
+
+    // aggiungere un nuovo viaggio
+    const addTrip = (newTrip) => {
+        setTrips(prev => [...prev, newTrip]);
+    };
+
+    const addParticipantToTrip = (tripId, participantId) => {
+        // prevTrips è l’array attuale dei viaggi
+        setTrips(prevTrips =>
+        prevTrips.map(trip => {
+            if (trip.id === tripId) {
+                // verifica se il partecipante è già presente
+                if (!trip.participants.includes(participantId)) {
+                    // crea un nuovo oggetto viaggio con il nuovo partecipante aggiunto
+                    return { ...trip, participants: [...trip.participants, participantId] };
+                }
+            }
+            // ritorna il viaggio invariato se non corrisponde o se il partecipante è già presente
+            return trip;
+        })
+        );
+    };
+
+
+    return (
+        <TripContext.Provider value={{ 
+            trips, 
+            setTrips, 
+            participants, 
+            setParticipants, 
+            getParticipantsByTripId,
+            addParticipant,
+            addTrip,
+            addParticipantToTrip 
+        }}>
+        {children}
+        </TripContext.Provider>
+    );
 };
 
 
