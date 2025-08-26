6db0149ebb1c233f31f927e04bea952e0e01a479
GianniC88 <gianni.colucci.88@gmail.com>
2025-08-26 11:48:16 +0200

    fix: update Jumbotron text and CTA on HomePage component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,7 +4,7 @@ import Jumbotron from "../components/Jumbotron";
 export default function HomePage() {
     return (
         <>
-            <Jumbotron title="Boo Road Travel" text="Scopri le migliori destinazioni e pianifica il Tuo Viaggio" cta="Scopri i Viaggi" img_url="https://esempio.com/banner.jpg" />
+            <Jumbotron title="Boo Road Travel" text="Inostri Viaggi" cta="Tutti i Viaggi" img_url="https://esempio.com/banner.jpg" />
             <TripsList />
         </>
     )
