de27f207d32948c94a3505ed4af979f7105d4b0d
Cristian Buonocore <cristian.buonocore680@gmail.com>
2025-08-26 11:10:56 +0200

    added footer with information about the site

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,125 @@
-export default function Footer(){
-    return(
+export default function Footer() {
+    return (
         <>
-            <h1>FOOTER</h1>
+            <footer className="bg-primary text-white text-center py-4 mt-auto">
+                <div className="container">
+                    <p className="mb-0">&copy; {new Date().getFullYear()} BoolRoad</p>
+                    <div className="d-flex justify-content-md-center">
+                        <ul className="list-inline mt-2 d-flex flex-column p-3 ">
+                            <h6><strong>Programma Viaggi!</strong></h6>
+                            <li className="list-inline-item p-2">
+                                <a href="/privacy" className="text-white text-decoration-none">
+                                    Viaggi di gruppo Luglio
+                                </a>
+                            </li>
+                            <li className="list-inline-item p-2">
+                                <a href="/privacy" className="text-white text-decoration-none">
+                                    Last Minute: Viaggi di gruppo Agosto
+                                </a>
+                            </li>
+                            <li className="list-inline-item p-2">
+                                <a href="/privacy" className="text-white text-decoration-none">
+                                    Viaggi di gruppo Settembre
+                                </a>
+                            </li>
+                            <li className="list-inline-item p-2">
+                                <a href="/privacy" className="text-white text-decoration-none">
+                                    Viaggi di gruppo Ottobre
+                                </a>
+                            </li>
+                            <li className="list-inline-item p-2">
+                                <a href="/privacy" className="text-white text-decoration-none">
+                                    Viaggi di gruppo Novembre
+                                </a>
+                            </li>
+                            <li className="list-inline-item p-2">
+                                <a href="/privacy" className="text-white text-decoration-none">
+                                    Viaggi di gruppo Dicembre
+                                </a>
+                            </li>
+                        </ul>
+                        <ul className="list-inline mt-2 d-flex flex-column p-3 ">
+                            <h6><strong>Il Team</strong></h6>
+                            <li className="list-inline-item p-2">
+                                <a href="/privacy" className="text-white text-decoration-none">
+                                    Edoardo
+                                </a>
+                            </li>
+                            <li className="list-inline-item p-2">
+                                <a href="/privacy" className="text-white text-decoration-none">
+                                    Rolinda
+                                </a>
+                            </li>
+                            <li className="list-inline-item p-2">
+                                <a href="/privacy" className="text-white text-decoration-none">
+                                    Francesco
+                                </a>
+                            </li>
+                            <li className="list-inline-item p-2">
+                                <a href="/privacy" className="text-white text-decoration-none">
+                                    Giovanni
+                                </a>
+                            </li>
+                            <li className="list-inline-item p-2">
+                                <a href="/privacy" className="text-white text-decoration-none">
+                                    Cristian
+                                </a>
+                            </li>
+                        </ul>
+                        <ul className="list-inline mt-2 d-flex flex-column p-3 ">
+                            <h6><strong>Info utili</strong></h6>
+                            <li className="list-inline-item p-2">
+                                <a href="/privacy" className="text-white text-decoration-none">
+                                    Faq
+                                </a>
+                            </li>
+                            <li className="list-inline-item p-2">
+                                <a href="/privacy" className="text-white text-decoration-none">
+                                    Il Blog
+                                </a>
+                            </li>
+                            <li className="list-inline-item p-2">
+                                <a href="/privacy" className="text-white text-decoration-none">
+                                    Contattaci
+                                </a>
+                            </li>
+                            <li className="list-inline-item p-2">
+                                <a href="/privacy" className="text-white text-decoration-none">
+                                    Termini e condizioni
+                                </a>
+                            </li>
+                            <li className="list-inline-item p-2">
+                                <a href="/privacy" className="text-white text-decoration-none">
+                                    Condizioni generali di <br />vendita pacchetti turistici
+                                </a>
+                            </li>
+                        </ul>
+                        <ul className="list-inline mt-2 d-flex flex-column p-3 ">
+                            <h6><strong>SEGUICI!</strong></h6>
+                            <li className="list-inline-item p-2">
+                                <a href="/privacy" className="text-white text-decoration-none">
+                                    <i class="bi bi-instagram"></i>
+                                </a>
+                            </li>
+                            <li className="list-inline-item p-2">
+                                <a href="/privacy" className="text-white text-decoration-none">
+                                    <i class="bi bi-facebook"></i>
+                                </a>
+                            </li>
+                            <li className="list-inline-item p-2">
+                                <a href="/privacy" className="text-white text-decoration-none">
+                                    <i class="bi bi-whatsapp"></i>
+                                </a>
+                            </li>
+                            <li className="list-inline-item p-2">
+                                <a href="/privacy" className="text-white text-decoration-none">
+                                    <i class="bi bi-discord"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </footer>
         </>
     )
 }
