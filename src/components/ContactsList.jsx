efaf273897caa1cf7832f5b7f22c5bc58376b918
rolindaz <rolinda.zamboni@gmail.com>
2025-08-27 15:30:42 +0200

    feat: add search functionality to ContactsList for filtering participants

--- src/components/ContactsList.jsx
+++ src/components/ContactsList.jsx
@@ -1,16 +1,36 @@
+import { useState } from "react";
 import ContactCard from "./ContactCard";
 
 export default function ContactsList({ participants }) {
 
+    const [searchTerm, setSearchTerm] = useState("");
+    const filteredParticipants = participants.filter((participant) =>
+        participant.firstName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        participant.lastName.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
     return (
         <>
-            <h3>
-                Partecipanti:
-            </h3>
+            <div className="row">
+                <div className="col">
+                    <h3>
+                        Partecipanti
+                    </h3>
+                </div>
+                <div className="col">
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Cerca per partecipante..."
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                    />
+                </div>
+            </div>
             <div className="participantsTable">
                 <div className="accordion accordion-flush" id="accordionFlushExample">
-                    {participants && participants.length > 0 ? (
-                        participants.map((p, index) => (
+                    {filteredParticipants && filteredParticipants.length > 0 ? (
+                        filteredParticipants.map((p, index) => (
                             <>
                                 <ContactCard key={p.id} p={p} index={index} />
                             </>
