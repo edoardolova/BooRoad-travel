9be725c6b5007d807cbfc0dc176c59f0e7f2e716
Cristian Buonocore <cristian.buonocore680@gmail.com>
2025-08-27 21:50:13 +0200

    edit link footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -37,27 +37,27 @@ export default function Footer() {
                         <ul className="list-inline mt-2 d-flex flex-column p-3 ">
                             <h6><strong>Il Team</strong></h6>
                             <li className="list-inline-item p-2">
-                                <a href="/privacy" className="text-white text-decoration-none">
+                                <a href="" className="text-white text-decoration-none">
                                     Edoardo
                                 </a>
                             </li>
                             <li className="list-inline-item p-2">
-                                <a href="/privacy" className="text-white text-decoration-none">
+                                <a href="" className="text-white text-decoration-none">
                                     Rolinda
                                 </a>
                             </li>
                             <li className="list-inline-item p-2">
-                                <a href="/privacy" className="text-white text-decoration-none">
+                                <a href="" className="text-white text-decoration-none">
                                     Francesco
                                 </a>
                             </li>
                             <li className="list-inline-item p-2">
-                                <a href="/privacy" className="text-white text-decoration-none">
+                                <a href="" className="text-white text-decoration-none">
                                     Giovanni
                                 </a>
                             </li>
                             <li className="list-inline-item p-2">
-                                <a href="/privacy" className="text-white text-decoration-none">
+                                <a href="" className="text-white text-decoration-none">
                                     Cristian
                                 </a>
                             </li>
@@ -65,27 +65,27 @@ export default function Footer() {
                         <ul className="list-inline mt-2 d-flex flex-column p-3 ">
                             <h6><strong>Info utili</strong></h6>
                             <li className="list-inline-item p-2">
-                                <a href="/privacy" className="text-white text-decoration-none">
+                                <a href="" className="text-white text-decoration-none">
                                     Faq
                                 </a>
                             </li>
                             <li className="list-inline-item p-2">
-                                <a href="/privacy" className="text-white text-decoration-none">
+                                <a href="" className="text-white text-decoration-none">
                                     Il Blog
                                 </a>
                             </li>
                             <li className="list-inline-item p-2">
-                                <a href="/privacy" className="text-white text-decoration-none">
+                                <a href="" className="text-white text-decoration-none">
                                     Contattaci
                                 </a>
                             </li>
                             <li className="list-inline-item p-2">
-                                <a href="/privacy" className="text-white text-decoration-none">
+                                <a href="" className="text-white text-decoration-none">
                                     Termini e condizioni
                                 </a>
                             </li>
                             <li className="list-inline-item p-2">
-                                <a href="/privacy" className="text-white text-decoration-none">
+                                <a href="" className="text-white text-decoration-none">
                                     Condizioni generali di <br />vendita pacchetti turistici
                                 </a>
                             </li>
@@ -93,22 +93,22 @@ export default function Footer() {
                         <ul className="list-inline mt-2 d-flex flex-column p-3 ">
                             <h6><strong>SEGUICI!</strong></h6>
                             <li className="list-inline-item p-2">
-                                <a href="/privacy" className="text-white text-decoration-none">
+                                <a href="" className="text-white text-decoration-none">
                                     <i className="bi bi-instagram"></i>
                                 </a>
                             </li>
                             <li className="list-inline-item p-2">
-                                <a href="/privacy" className="text-white text-decoration-none">
+                                <a href="" className="text-white text-decoration-none">
                                     <i className="bi bi-facebook"></i>
                                 </a>
                             </li>
                             <li className="list-inline-item p-2">
-                                <a href="/privacy" className="text-white text-decoration-none">
+                                <a href="" className="text-white text-decoration-none">
                                     <i className="bi bi-whatsapp"></i>
                                 </a>
                             </li>
                             <li className="list-inline-item p-2">
-                                <a href="/privacy" className="text-white text-decoration-none">
+                                <a href="" className="text-white text-decoration-none">
                                     <i className="bi bi-discord"></i>
                                 </a>
                             </li>
