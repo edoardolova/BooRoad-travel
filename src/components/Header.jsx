d214cd6720272c7bf04fb24e5af8d6c0c2f5df6b
coluccifrancesco <coluccif2002@gmail.com>
2025-08-28 10:30:57 +0200

    fix: correct brand name spelling in Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header(){
             <nav className="bg-white d-flex justify-content-between align-items-center p-3 position-fixed z-3 w-100">
                 <div className='d-flex align-items-center gap-2'>
                     <Link to={'/'}><img src='/boo_road_logo.png' alt='Boo Road Travel' className='logo-img' /></Link>
-                    <Link to={'/'}><h2 className='text-black mb-0' >Boo Road Travel</h2></Link>
+                    <Link to={'/'}><h2 className='text-black mb-0' >BooRoad Travel</h2></Link>
                 </div>
 
                 {/* inserire altri elementi della nav */}
